919f6d869f0ab5e8191a9860c0484a7ddb3f6893
hurnhu <hurnhu5622@gmail.com>
2018-10-04 18:24:40 +0200

    added details of specific sub

--- src/account/subaccount/operations.rs
+++ src/account/subaccount/operations.rs
@@ -1,8 +1,8 @@
 use dotenv::var;
-use generic_post;
 use generic_get;
-use std::collections::HashMap;
+use generic_post;
 use serde_json::{Error, Value};
+use std::collections::HashMap;
 pub fn create_subaccount(name: String, enabled: String) -> Result<Value, &'static str> {
     let chars_to_trim: &[char] = &['"'];
     let mut map: HashMap<&str, &str> = HashMap::new();
@@ -35,13 +35,22 @@ pub fn modify_subaccount(
         &map,
     )
 }
+
 pub fn details_all(limit: u16, offset: u16) -> Result<Value, &'static str> {
-    generic_get(
-        format!(
-            "https://api.plivo.com/v1/Account/{}/Subaccount/?limit={}&offset={}",
-            &var("PLIVO_AUTH_ID").unwrap(), limit, offset
-        )
-    )
+    generic_get(format!(
+        "https://api.plivo.com/v1/Account/{}/Subaccount/?limit={}&offset={}",
+        &var("PLIVO_AUTH_ID").unwrap(),
+        limit,
+        offset
+    ))
+}
+
+pub fn details_specific(subauth_id: String) -> Result<Value, &'static str> {
+    generic_get(format!(
+        "https://api.plivo.com/v1/Account/{}/Subaccount/{}/",
+        &var("PLIVO_AUTH_ID").unwrap(),
+        subauth_id
+    ))
 }
 
 pub fn delete() -> String {
