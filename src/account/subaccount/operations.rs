0ec9c6fd88d68ff54c8732211a188c0c07f1b086
hurnhu <hurnhu5622@gmail.com>
2018-10-04 15:58:28 +0200

    moved around some functions

--- src/account/subaccount/operations.rs
+++ src/account/subaccount/operations.rs
@@ -1,8 +1,42 @@
-pub fn create() -> String {
-    "sub acc create".to_string()
+use dotenv::var;
+use generic_post;
+use reqwest::Client;
+use reqwest::StatusCode;
+use serde_json;
+use serde_json::{Error, Value};
+use std::collections::HashMap;
+
+pub fn create_subaccount(name: String, enabled: String) -> Result<Value, &'static str> {
+    let chars_to_trim: &[char] = &['"'];
+    let mut map: HashMap<&str, &str> = HashMap::new();
+    map.insert("name", name.trim_matches(chars_to_trim));
+    map.insert("enabled", enabled.trim_matches(chars_to_trim));
+    generic_post(
+        format!(
+            "https://api.plivo.com/v1/Account/{}/Subaccount/",
+            &var("PLIVO_AUTH_ID").unwrap()
+        ),
+        &map,
+    )
 }
-pub fn modify() -> String {
-    "sub acc modify".to_string()
+
+pub fn modify_subaccount(
+    name: String,
+    enabled: String,
+    subauth_id: String,
+) -> Result<Value, &'static str> {
+    let chars_to_trim: &[char] = &['"'];
+    let mut map: HashMap<&str, &str> = HashMap::new();
+    map.insert("name", name.trim_matches(chars_to_trim));
+    map.insert("enabled", enabled.trim_matches(chars_to_trim));
+    generic_post(
+        format!(
+            "https://api.plivo.com/v1/Account/{AUTH}/Subaccount/{SUBAUTH}",
+            AUTH = &var("PLIVO_AUTH_ID").unwrap(),
+            SUBAUTH = subauth_id.trim_matches(chars_to_trim)
+        ),
+        &map,
+    )
 }
 pub fn delete() -> String {
     "sub acc delete".to_string()
